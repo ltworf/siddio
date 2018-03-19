415daf9c5196dcebeaca2faed352c898d249de2d
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2018-03-19 23:09:54 +0100

    Remove unused variable

--- panel/main.cpp
+++ panel/main.cpp
@@ -11,7 +11,6 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
 
-    HomeControlClient cl;
     QQmlApplicationEngine engine;
     qmlRegisterType<HomeControlClient>("siddio.control", 1, 0, "HomeControlClient");
     qmlRegisterType<AudioPlayer>("siddio.control", 1, 0, "MpvPlayer");
