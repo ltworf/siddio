7a7f47b99ab4924599918eb13d9ed108510538a7
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2019-01-30 00:09:37 +0100

    Store settings somewhere nice

--- panel/main.cpp
+++ panel/main.cpp
@@ -29,6 +29,8 @@ int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
+    app.setOrganizationName("SiegeSoftware");
+    app.setApplicationName("siddio-panel");
 
     QQmlApplicationEngine engine;
     qmlRegisterType<HomeControlClient>("siddio.control", 1, 0, "HomeControlClient");
